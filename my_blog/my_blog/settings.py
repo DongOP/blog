0a06d68a3085fec35f54b7b7ff2691390d38fd68
DongOP <donking0030@gmail.com>
2016-04-28 17:00:11 +0800

    add aboutme

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -123,9 +123,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATICFILES_DIRS = [
-    r'E:\blog\my_blog\my_blog\static',
-]
+#static path for home
+# STATICFILES_DIRS = [
+#     r'E:\blog\my_blog\my_blog\static',
+# ]
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(os.path.dirname(__file__), 'static'),
+    ]
